5982fe6c4c27d6a7627976e96b2de134e8d91815
shy-lemon <shy@lemoncloud.io>
2025-03-24 20:31:08 +0900

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { LocationModule } from './location/location.module';
+import { ProposalModule } from './proposal/proposal.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, LocationModule, ProposalModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
