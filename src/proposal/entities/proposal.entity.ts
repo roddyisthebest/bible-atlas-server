df8e48af0885f79165c4a796f5e652cf46d94cf8
shy-lemon <shy@lemoncloud.io>
2025-03-24 21:34:29 +0900

    feat: add proposal entity

--- src/proposal/entities/proposal.entity.ts
+++ src/proposal/entities/proposal.entity.ts
@@ -1,1 +1,34 @@
-export class Proposal {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum ProprosalType {
+  CREATE,
+  UPDATE,
+  DELETE,
+}
+
+@Entity()
+export class Proposal {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: ProprosalType;
+
+  @Column()
+  content: string;
+
+  @Column()
+  comment: string;
+
+  @Column({ default: 0 })
+  agreeCount: number;
+
+  @Column({ default: 0 })
+  disagreeCount: number;
+
+  @Column()
+  newLatitude: number;
+
+  @Column()
+  newLongitude: number;
+}
