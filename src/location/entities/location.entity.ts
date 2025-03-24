8cb98f605f90faeaf5124ecc95f16b87ba496102
shy-lemon <shy@lemoncloud.io>
2025-03-24 20:57:27 +0900

    feat: add location entity

--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -1,1 +1,23 @@
-export class Location {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['latitude', 'longitude'])
+export class Location {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column({ default: 0 })
+  likeCount: number;
+}
