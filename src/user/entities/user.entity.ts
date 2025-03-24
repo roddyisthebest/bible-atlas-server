aa07218e2b5832c1c304fcaf9a4a83f22b301395
shy-lemon <shy@lemoncloud.io>
2025-03-24 20:48:07 +0900

    feat: add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+export enum Role {
+  SUPER,
+  POWER_EXPERT,
+  EXPERT,
+  USER,
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  @Exclude({ toPlainOnly: true })
+  password: string;
+
+  @Column({ enum: Role, default: Role.USER })
+  role: Role;
+}
