2d79040fdb1d3ba625792a692f15a162666494c0
shy-lemon <shy@lemoncloud.io>
2025-03-26 14:27:34 +0900

    refactor: clean up unused code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,26 +20,6 @@ export class AuthService {
     private readonly userSerice: UserService,
   ) {}
 
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
-
   async authenticate(email: string, password: string) {
     const user = await this.userRepository.findOne({ where: { email } });
 
