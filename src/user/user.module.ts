ee554820f78c0c8dd0f7ee6245a2c6f81374f44d
shy-lemon <shy@lemoncloud.io>
2025-03-31 18:24:22 +0900

    feat: get saved, liked locations

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,20 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Proposal } from 'src/proposal/entities/proposal.entity';
+import { UserLocationLike } from './entities/user-location-like.entity';
+import { CommonModule } from 'src/common/common.module';
+import { UserLocationSave } from './entities/user-location-save.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Proposal])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Proposal,
+      UserLocationLike,
+      UserLocationSave,
+    ]),
+    CommonModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,6 +11,10 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { envVariables } from 'src/common/const/env.const';
+import { CommonService } from 'src/common/common.service';
+import { UserLocationLike } from './entities/user-location-like.entity';
+import { FindAllDto } from 'src/location/dto/find-all.dto';
+import { UserLocationSave } from './entities/user-location-save.entity';
 
 @Injectable()
 export class UserService {
@@ -18,6 +22,11 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly configService: ConfigService,
+    private readonly commonService: CommonService,
+    @InjectRepository(UserLocationLike)
+    private readonly userLocationLikeRepository: Repository<UserLocationLike>,
+    @InjectRepository(UserLocationSave)
+    private readonly userLocationSaveRepository: Repository<UserLocationSave>,
   ) {}
 
   async create(createUserDto: CreateUserDto, isSns: boolean = false) {
@@ -76,5 +85,65 @@ export class UserService {
 
     await this.userRepository.delete({ id });
     return id;
+  }
+
+  async getMySavedLocations(
+    { page, limit, query }: FindAllDto,
+    userId: number,
+  ) {
+    const qb = this.userLocationSaveRepository
+      .createQueryBuilder('uls')
+      .leftJoinAndSelect('uls.user', 'user')
+      .leftJoinAndSelect('uls.location', 'location')
+      .where('user.id = :userId', { userId });
+
+    if (query) {
+      qb.where('location.name ILIKE :query', { query: `%${query}%` });
+    }
+
+    this.commonService.applyPagePaginationParamsToQb(qb, { limit, page });
+
+    qb.orderBy('location.createdAt', 'DESC');
+
+    const [data, total] = await qb.getManyAndCount();
+
+    const filteredData = data.map((d) => d.location);
+
+    return {
+      total,
+      page,
+      limit,
+      data: filteredData,
+    };
+  }
+
+  async getMyLikedLocations(
+    { page, limit, query }: FindAllDto,
+    userId: number,
+  ) {
+    const qb = this.userLocationLikeRepository
+      .createQueryBuilder('ull')
+      .leftJoinAndSelect('ull.user', 'user')
+      .leftJoinAndSelect('ull.location', 'location')
+      .where('user.id = :userId', { userId });
+
+    if (query) {
+      qb.where('location.name ILIKE :query', { query: `%${query}%` });
+    }
+
+    this.commonService.applyPagePaginationParamsToQb(qb, { limit, page });
+
+    qb.orderBy('location.createdAt', 'DESC');
+
+    const [data, total] = await qb.getManyAndCount();
+
+    const filteredData = data.map((d) => d.location);
+
+    return {
+      total,
+      page,
+      limit,
+      data: filteredData,
+    };
   }
 }
