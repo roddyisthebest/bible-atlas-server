44a0ca1bc9db6005344cf768d42137ebb2aa4a23
shy-lemon <shy@lemoncloud.io>
2025-04-01 15:59:06 +0900

    feat: prepare push notification structure for recent notifications and reports

--- src/common/task.service.ts
+++ src/common/task.service.ts
@@ -5,11 +5,14 @@ import {
   DEFAULT_SYNC_LOCATION_LIKE_CRON,
   DEFAULT_SYNC_PROPOSAL_CRON,
 } from './const/task.const';
-import { Repository } from 'typeorm';
+import { Between, DataSource, Repository } from 'typeorm';
 
 import { Proposal } from 'src/proposal/entities/proposal.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserLocationLike } from 'src/user/entities/user-location-like.entity';
+import { Notification } from 'src/notification/entities/notification.entity';
+
+import { Role, User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class TaskService {
@@ -20,6 +23,11 @@ export class TaskService {
     private readonly proposalRepository: Repository<Proposal>,
     @InjectRepository(UserLocationLike)
     private readonly userLocationLikeRepository: Repository<UserLocationLike>,
+    @InjectRepository(Notification)
+    private readonly notificationRepository: Repository<Notification>,
+    private readonly dataSource: DataSource,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   @Cron(process.env.SYNC_PROPOSAL_CRON || DEFAULT_SYNC_PROPOSAL_CRON)
@@ -58,6 +66,78 @@ export class TaskService {
       this.logger.log('location like counts updated successfully');
     } catch (error) {
       this.logger.error('❌ Failed to update location like counts:', error);
+    }
+  }
+
+  async sendPushForNotification() {
+    const now = new Date();
+    const fifteenMinutesAgo = new Date(now.getTime() - 15 * 60 * 1000);
+
+    try {
+      const recentNotifications = await this.notificationRepository.find({
+        where: {
+          createdAt: Between(fifteenMinutesAgo, now),
+        },
+        relations: ['user'],
+      });
+
+      if (recentNotifications.length === 0) {
+        this.logger.log('No recent notifications to push');
+        return;
+      }
+
+      const users = recentNotifications.map(
+        (notification) => notification.user,
+      );
+      // TODO: 후에 푸시 알림 구현
+    } catch (error) {
+      this.logger.error('❌ Failed to send push notifications:', error);
+    }
+  }
+
+  async sendPushForReportToAdmin() {
+    const now = new Date();
+    const fifteenMinutesAgo = new Date(now.getTime() - 15 * 60 * 1000);
+
+    try {
+      const recentReports = await this.dataSource.query(
+        `
+  SELECT 
+    id,
+    'location' AS type,
+    "userId",
+    "createdAt"
+  FROM user_location_report
+  WHERE "createdAt" BETWEEN $1 AND $2
+
+  UNION ALL
+
+  SELECT 
+    id,
+    'proposal' AS type,
+    "userId",
+    "createdAt"
+  FROM user_proposal_report
+  WHERE "createdAt" BETWEEN $1 AND $2
+
+  ORDER BY "createdAt" DESC
+  LIMIT 10
+`,
+        [fifteenMinutesAgo, now],
+      );
+
+      if (recentReports?.length === 0) {
+        this.logger.log('No recent reports to push');
+        return;
+      }
+
+      const admin = await this.userRepository.findOne({
+        where: { role: Role.SUPER },
+      });
+
+      // TODO: 후에 푸시 알림 구현
+    } catch (error) {
+      this.logger.error('❌ Failed to send push notifications:', error);
     }
   }
 }
