6458dafc98f2bb05abe547470feabbc7bfff21a5
shy-lemon <shy@lemoncloud.io>
2025-05-19 18:24:03 +0900

    feat: get profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,6 +12,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { MinimumRole } from 'src/auth/decorator/minimun-role.decorator';
 import { Role } from './entities/user.entity';
+import { UserId } from 'src/common/decorator/user-id.decorator';
 
 @Controller('user')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -27,6 +28,11 @@ export class UserController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  @Get('me')
+  findMyProfile(@UserId() userId: number) {
+    return this.userService.findOne(userId);
   }
 
   @MinimumRole(Role.SUPER)
